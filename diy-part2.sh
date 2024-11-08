630976918d5ecec31e305192d3bcc1ca761bd1d0
haoxiandi <145139578+haoxiandi@users.noreply.github.com>
2024-11-08 23:30:36 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -8,8 +8,8 @@
 #
 # This is free software, licensed under the MIT License.
 # See /LICENSE for more information.
-#
-
+git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
+git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 
